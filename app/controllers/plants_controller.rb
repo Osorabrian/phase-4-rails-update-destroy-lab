f461fee6f4a0fce0ec29b1b5873fbdba4ae2b653
Osorabrian <osorabrian@gmail.com>
2023-03-14 14:55:43 +0300

    completed assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,5 +1,7 @@
 class PlantsController < ApplicationController
 
+rescue_from ActiveRecord::RecordNotFound, with: :render_no_plant
+
   # GET /plants
   def index
     plants = Plant.all
@@ -8,7 +10,7 @@ class PlantsController < ApplicationController
 
   # GET /plants/:id
   def show
-    plant = Plant.find_by(id: params[:id])
+    plant = find_plant
     render json: plant
   end
 
@@ -18,9 +20,31 @@ class PlantsController < ApplicationController
     render json: plant, status: :created
   end
 
+  def update
+    plant = find_plant
+    plant.update(plant_params)
+    render json: plant, status: :created
+  end
+
+  def destroy
+    plant = find_plant
+    plant.destroy
+    render json: {}
+  end
+
   private
 
   def plant_params
     params.permit(:name, :image, :price, :is_in_stock)
   end
+
+  def find_plant
+    Plant.find(params[:id])
+  end
+
+  def render_no_plant
+    render json: {error: "error no plant"}
+  end
+
+
 end
